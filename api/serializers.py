539146247c6ba5088160dadd8d28ae30f1618ce4
Ricardo Lopes Pires <unicodireito@gmail.com>
2024-03-21 18:35:47 -0300

    Atualização do projeto

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from quiz.models import Quiz, Question, Answer,  UsersAnswer, QuizTaker
-from accounts.models import Perfil, Situacao, User
-from rest_framework import serializers
-
-
-
-
--- economia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EconomiaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'economia'
